05a474d7fa917ea49a61e172d7174a9389bcdf9b
J_Mont <josemont7902@gmail.com>
2023-05-01 21:03:23 -0600

    First_Version

---
+++ Prueba_Arduino/Prueba_Arduino.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		Prueba_Arduino.ino
+ Created:	29/04/2023 13:53:39
+ Author:	josem
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	Serial.begin(115200);
+	Serial.println("Hola Mundo!");
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
